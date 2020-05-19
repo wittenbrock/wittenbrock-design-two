d31d5a62d30d266dbb2ac541410d7a64f79c74dc
William Wittenbrock <william.wittenbrock@gmail.com>
2020-05-19 08:39:40 -0700

    Finish section two

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import SEO from '../components/seo';
 import SectionOne from '../layouts/section-one';
+import SectionTwo from '../layouts/section-two';
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
     <main>
       <SectionOne />
+      <SectionTwo />
     </main>
   </>
 );
