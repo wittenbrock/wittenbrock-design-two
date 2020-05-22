b32cde3c00c980e8cb23869ca7c62eaacb462de0
William Wittenbrock <william.wittenbrock@gmail.com>
2020-05-22 08:46:13 -0700

    DRY improvement - social media icon component

--- src/components/social-media-icon/index.js
+++ src/components/social-media-icon/index.js
@@ -6,56 +6,47 @@ import linkedInSvg from '../../images/linkedIn.svg';
 import gitHubSvg from '../../images/github.svg';
 import jelloHorizontalOnHover from './style';
 
+const linkedInSpecs = {
+  url: `https://www.linkedin.com/in/william-wittenbrock/`,
+  text: `Visit William's LinkedIn profile.`,
+  image: linkedInSvg,
+  imageAlt: `LinkedIn Icon`,
+};
+
+const gitHubSpecs = {
+  url: `https://github.com/wittenbrock`,
+  text: `Visit William's Github profile.`,
+  image: gitHubSvg,
+  imageAlt: `Github Icon`,
+};
+
+// A function that returns a social media icon wrapped in HTML
+const createSocialMediaIcon = specs => {
+  const { url, text, image, imageAlt } = specs;
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      css={[
+        tw`inline-block hover:cursor-pointer no-underline h-6 w-6`,
+        jelloHorizontalOnHover,
+      ]}
+    >
+      <span css={tw`sr-only`}>{text}</span>
+      <img src={image} alt={imageAlt} aria-hidden="true" focusable="false" />
+    </a>
+  );
+};
+
+// React component - render the social media icon and its HTML
 const SocialMediaIcon = ({ linkedIn, gitHub }) => {
-  // Function that creates either a LinkedIn or a GitHub icon based on props
-  const createSocialMediaIcon = () => {
-    if (linkedIn) {
-      return (
-        <a
-          href="https://www.linkedin.com/in/william-wittenbrock/"
-          target="_blank"
-          rel="noopener noreferrer"
-          css={[
-            tw`inline-block hover:cursor-pointer no-underline h-6 w-6`,
-            jelloHorizontalOnHover,
-          ]}
-        >
-          <span css={tw`sr-only`}>Visit William's LinkedIn profile.</span>
-          <img
-            src={linkedInSvg}
-            alt="LinkedIn icon"
-            aria-hidden="true"
-            focusable="false"
-          />
-        </a>
-      );
-    }
-
-    if (gitHub) {
-      return (
-        <a
-          href="https://github.com/wittenbrock"
-          target="_blank"
-          rel="noopener noreferrer"
-          css={[
-            tw`inline-block hover:cursor-pointer no-underline h-6 w-6`,
-            jelloHorizontalOnHover,
-          ]}
-        >
-          <span css={tw`sr-only`}>Visit William's Github profile.</span>
-          <img
-            src={gitHubSvg}
-            alt="Github icon"
-            aria-hidden="true"
-            focusable="false"
-          />
-        </a>
-      );
-    }
-  };
-
-  // Render the icon UI
-  return <div>{createSocialMediaIcon()}</div>;
+  if (linkedIn) {
+    return <div>{createSocialMediaIcon(linkedInSpecs)}</div>;
+  }
+  if (gitHub) {
+    return <div>{createSocialMediaIcon(gitHubSpecs)}</div>;
+  }
 };
 
 export default SocialMediaIcon;
