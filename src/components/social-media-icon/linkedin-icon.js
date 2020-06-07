21d8c87f2b6811ab00f1a87cbdcf0d1fa733975c
William Wittenbrock <william.wittenbrock@gmail.com>
2020-06-07 15:23:21 -0700

    Make social media icons React components

---
+++ src/components/social-media-icon/linkedin-icon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'twin.macro';
+
+export default () => (
+  <svg
+    tw="fill-current"
+    viewBox="0 0 24 24"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M24 14.594v8.873h-5.144v-8.28c0-2.08-.744-3.5-2.606-3.5a2.815 2.815 0 0 0-2.639 1.882 3.512 3.512 0 0 0-.171 1.254v8.642H8.294V7.992h5.146v2.193l-.034.05h.034v-.05a5.107 5.107 0 0 1 4.637-2.557C21.462 7.63 24 9.84 24 14.594zM2.912.533a2.681 2.681 0 1 0-.069 5.348h.035A2.682 2.682 0 1 0 2.912.533zM.306 23.467H5.45V7.993H.306z" />
+  </svg>
+);
