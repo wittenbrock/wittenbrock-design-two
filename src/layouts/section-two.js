47d81d468c79e8ff320a6343c09031fe4b340056
William Wittenbrock <william.wittenbrock@gmail.com>
2020-06-16 21:25:38 -0700

    Clean up twin macro syntax

--- src/layouts/section-two.js
+++ src/layouts/section-two.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import tw from 'twin.macro';
+import 'twin.macro';
 
 import { ButtonDown } from '../components/button-down/button-down';
 
 const SectionTwo = () => (
   <section
     id="introduction"
-    css={tw`w-full min-h-screen py-24 xl:py-32 bg-indigo-darkest flex flex-col justify-center items-center`}
+    tw="w-full min-h-screen py-24 xl:py-32 bg-indigo-darkest flex flex-col justify-center items-center"
   >
-    <header css={tw`text-center text-white mb-32 md:mb-40 xl:mb-48`}>
+    <header tw="text-center text-white mb-32 md:mb-40 xl:mb-48">
       <h2
-        css={tw`font-display font-bold text-3xl md:text-4xl xl:text-5xl mb-4 md:mb-8 xl:mb-12`}
+        tw="font-display font-bold text-3xl md:text-4xl xl:text-5xl mb-4 md:mb-8 xl:mb-12"
       >
         <span>I design and code </span>
-        <span css={tw`block sm:inline`}>beautiful websites.</span>
+        <span tw="block sm:inline">beautiful websites.</span>
       </h2>
       <p
-        css={tw`font-body font-semibold text-lg md:text-xl xl:text-2xl mb-4 md:mb-8 xl:mb-12`}
+        tw="font-body font-semibold text-lg md:text-xl xl:text-2xl mb-4 md:mb-8 xl:mb-12"
       >
         I am William Wittenbrock.
       </p>
-      <p css={tw`font-body text-lg lg:text-xl`}>I love what I do.</p>
+      <p tw="font-body text-lg lg:text-xl">I love what I do.</p>
     </header>
     <ButtonDown scrollTo={'#contact'} />
   </section>
