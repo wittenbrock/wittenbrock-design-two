51c7a3bfae522e5a1520d6dac503eff61646224a
William Wittenbrock <william.wittenbrock@gmail.com>
2020-08-25 20:11:52 -0700

    Add gatsby plugin netlfiy

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,6 +90,7 @@ module.exports = {
         icon: `src/images/favicon.png`,
       },
     },
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
