c2efe159870f4d23a1fabe1729d47beae81d0607
William Wittenbrock <william.wittenbrock@gmail.com>
2020-11-26 14:50:16 -0800

    Add and configure eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,6 +121,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-eslint`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
