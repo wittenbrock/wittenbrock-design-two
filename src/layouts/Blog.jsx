54eff9e36876c27170b1659a0918439c9563c6ae
William Wittenbrock <william.wittenbrock@gmail.com>
2020-11-27 20:16:51 -0800

    Configure husky 5

--- src/layouts/Blog.jsx
+++ src/layouts/Blog.jsx
@@ -27,7 +27,8 @@ export default function Blog() {
   return (
     <section
       id="blog"
-      t
+      tw="py-24 xl:py-32 px-4 sm:px-6 lg:pt-24 lg:pb-24 lg:px-8"
+    >
       <div tw="max-w-6xl mx-auto">
         <header tw="text-center text-white mb-24 max-w-lg xl:max-w-2xl mx-auto">
           <h2 tw="font-heading font-bold text-3xl sm:text-4xl xl:text-5xl mb-2 sm:mb-3 xl:mb-4">
