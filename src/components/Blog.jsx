c6060c1a2f1dc1214a3534ce73d92c3f4ed8be83
William Wittenbrock <william.wittenbrock@gmail.com>
2021-04-07 20:00:43 -0700

    Add missing id=blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -8,7 +8,7 @@ export default function Blog(props) {
   const { posts } = props;
 
   return (
-    <div tw="py-24 xl:py-32 px-4 sm:px-6 lg:pt-24 lg:pb-24 lg:px-8">
+    <div id="blog" tw="py-24 xl:py-32 px-4 sm:px-6 lg:pt-24 lg:pb-24 lg:px-8">
       <h2 tw="sr-only">Things I'm thinking about.</h2>
       <div tw="max-w-6xl mx-auto">
         <ul tw="relative grid gap-12 max-w-lg mx-auto lg:grid-cols-1 lg:max-w-none">
