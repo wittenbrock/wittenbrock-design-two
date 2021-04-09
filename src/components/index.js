356ff2e250f890d894743a3ce73c35f42443e3cd
William Wittenbrock <william.wittenbrock@gmail.com>
2021-04-08 22:53:39 -0700

    Delete <SocialMediaIcon /> component

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,9 @@
 import BlogCard from './BlogCard';
 import ContactForm from './contact-form/ContactForm';
-import SocialMediaIcon from './social-media-icon/SocialMediaIcon';
 import SEO from './seo/SEO';
 import Blog from './Blog';
 import Contact from './Contact';
 import Homepage from './Homepage';
 import PageWrapper from './PageWrapper';
 
-export {
-  BlogCard,
-  ContactForm,
-  SEO,
-  SocialMediaIcon,
-  Blog,
-  Contact,
-  Homepage,
-  PageWrapper,
-};
+export { BlogCard, ContactForm, SEO, Blog, Contact, Homepage, PageWrapper };
