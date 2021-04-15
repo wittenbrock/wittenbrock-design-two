84f4efa36458227f27937c0b79aa1db2bf4c84b2
William Wittenbrock <william.wittenbrock@gmail.com>
2021-04-14 22:47:26 -0700

    Animate <GrayDivider /> so that it draws down the page

--- src/components/GrayDivider.jsx
+++ src/components/GrayDivider.jsx
@@ -1,110 +1,139 @@
 import React from 'react';
+import 'twin.macro';
 import { keyframes } from '@emotion/react';
 import { css } from 'twin.macro';
 
 import tailwindColors from '../../tailwind-colors';
 
-// Create the animation that "draws" the gray dividing line
-const drawKeyframe = keyframes`
-  to {
-    stroke-dashoffset: 0;
-  }
-`;
-
-const draw = css`
-  animation: ${drawKeyframe} 3s 4s ease-in forwards;
-`;
-
-// Create the gray dividing line that runs down the center of the homepage
 export default function GrayDivider(props) {
-  const { dividerHeight, isFirstVisit } = props;
+  const { isFirstVisit, dividerHeight } = props;
 
-  // "Draw" the line if it's the user's first visit to the site
-  if (isFirstVisit) {
-    return (
-      <svg
-        style={{ position: 'absolute' }}
-        xmlns="http://www.w3.org/2000/svg"
-        stroke={tailwindColors.gray[600]}
-        height={dividerHeight}
-        width="6"
-      >
-        <defs>
-          <marker
-            id="arrowhead"
-            markerWidth="10"
-            markerHeight="7"
-            refX="0"
-            refY="3.5"
-            orient="auto"
-          >
-            <polygon points="0 0, 10 3.5, 0 7" />
-          </marker>
-        </defs>
-        <line
-          x1="0"
-          y1={dividerHeight}
-          x2="0"
-          y2="0"
-          strokeWidth={6}
-          stroke-dasharray={dividerHeight}
-          stroke-dashoffset={-dividerHeight}
-          css={draw}
-          marker-end="url(#arrowhead)"
-        />
-      </svg>
-    );
-  }
+  const draw = keyframes`
+    to {
+      stroke-dashoffset: 0;
+    }
+  `;
 
-  // If it's a subsequent visit, do not animate the line
-  return (
+  const drawHorizontalLine = css`
+    animation: ${draw} 0.25s linear 4.25s forwards;
+  `;
+
+  // Move the line and arrow outside of the svg's viewBox so that it's hidden
+  const verticalLineOffScreen = css`
+    transform: translateY(-${dividerHeight + 5}px);
+  `;
+
+  const moveVerticalLineKeyframes = keyframes`
+    from {
+      transform: translateY(-${dividerHeight + 5}px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  `;
+
+  // Move the line and arrow into the view box after waiting 4.5s
+  const moveVerticalLine = css`
+    animation: ${moveVerticalLineKeyframes} 4s ease-in 4.5s forwards;
+  `;
+
+  const GrayDivider = () => (
     <svg
-      style={{ position: 'absolute' }}
+      tw="absolute"
       xmlns="http://www.w3.org/2000/svg"
-      stroke={tailwindColors.gray[700]}
-      height={dividerHeight}
+      viewBox={`0 0 20 ${dividerHeight + 10}`}
       width="20"
-      viewbox={`0 0 20 ${dividerHeight}`}
+      height={dividerHeight}
     >
       <defs>
         <marker
-          id="dot"
-          viewBox="0 0 10 10"
-          refX="7"
-          refY="5"
-          markerWidth="5"
-          markerHeight="5"
-          orient="auto"
-          stroke={tailwindColors.gray[700]}
-          fill={tailwindColors.white}
-          strokeWidth={2}
-        >
-          <circle cx="5" cy="5" r="2" />
-        </marker>
-        <marker
           id="arrowhead"
           viewBox="0 0 10 10"
-          markerWidth="3"
-          markerHeight="3"
-          refX="3"
+          markerWidth="2"
+          markerHeight="2"
+          refX="5"
           refY="5"
-          orient="auto"
+          orient="-90deg"
           stroke="none"
           fill={tailwindColors.gray[700]}
         >
-          {/* points="0 0, 5 10, 10 0" */}
           <polygon points="0 5, 10 10, 10 0" />
         </marker>
       </defs>
       <line
+        id="horizontal-line"
+        x1="5"
+        y1="2"
+        x2="15"
+        y2="2"
+        fill="none"
+        stroke={tailwindColors.gray[700]}
+        strokeWidth="5"
+      />
+      <line
+        id="vertical-line"
         x1="10"
-        y1={dividerHeight - 5}
+        y1="4"
         x2="10"
-        y2="5"
-        strokeWidth={6}
-        marker-end="url(#dot)"
-        marker-start="url(#arrowhead)"
+        y2={dividerHeight}
+        fill="none"
+        stroke={tailwindColors.gray[700]}
+        strokeWidth="5"
+        markerEnd="url(#arrowhead)"
       />
     </svg>
   );
+
+  const GrayDividerAnimated = () => (
+    <svg
+      tw="absolute"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 20 ${dividerHeight + 10}`}
+      width="20"
+      height={dividerHeight}
+    >
+      <defs>
+        <marker
+          id="arrowhead"
+          viewBox="0 0 10 10"
+          markerWidth="2"
+          markerHeight="2"
+          refX="5"
+          refY="5"
+          orient="-90deg"
+          stroke="none"
+          fill={tailwindColors.gray[700]}
+        >
+          <polygon points="0 5, 10 10, 10 0" />
+        </marker>
+      </defs>
+      <line
+        id="horizontal-line"
+        x1="5"
+        y1="2"
+        x2="15"
+        y2="2"
+        fill="none"
+        stroke={tailwindColors.gray[700]}
+        strokeWidth="5"
+        strokeDashoffset="10"
+        strokeDasharray="10"
+        css={drawHorizontalLine}
+      />
+      <line
+        id="vertical-line"
+        x1="10"
+        y1="4"
+        x2="10"
+        y2={dividerHeight}
+        fill="none"
+        stroke={tailwindColors.gray[700]}
+        strokeWidth="5"
+        markerEnd="url(#arrowhead)"
+        css={[verticalLineOffScreen, moveVerticalLine]}
+      />
+    </svg>
+  );
+
+  return isFirstVisit ? <GrayDividerAnimated /> : <GrayDivider />;
 }
