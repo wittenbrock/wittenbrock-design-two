7e7a9d36e8162fee5fbc9dbd6ea42b93f50846f1
wittenbrock <william.wittenbrock@gmail.com>
2021-04-19 04:10:15 +0000

    Install gatsby-plugin-mailchimp

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,7 +128,12 @@ module.exports = {
     `gatsby-plugin-eslint`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        endpoint: process.env.MAILCHIMP_ENDPOINT,
+      },
+    },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
